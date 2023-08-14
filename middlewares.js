9327b08a92ada95fd5e959154f800460110dad16
Kleber Barreto de Macedo <kleber.barreto@msn.com>
2023-08-14 03:07:18 -0300

    Adicionadas correções para funcionar no node 14

--- middlewares.js
+++ middlewares.js
@@ -1,11 +1,23 @@
 const bodyParser = require('body-parser');
+const express =require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 
 module.exports = app => {
     app.set('port', 8005);
     app.set('json spaces', 4);
+    app.use(helmet());
+    app.use(cors({
+        origin: ['http://localhost:4200'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    }));
+    app.use(compression());
     app.use(bodyParser.json());
     app.use((req, res, next) => {
         delete req.body.id;
         next();
     });
+    app.use(express.static('public'));
 };
